6c409265b03e3f2c597680b786d4ea851b95771d
James Pereira dos Santos Amarante <james.pereira@academico.ifpb.edu.br>
2021-09-24 00:47:44 -0300

    add nova pasta build

--- build/server.js
+++ build/server.js
@@ -1,26 +1,20 @@
-const __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
-Object.defineProperty(exports, '__esModule', { value: true });
-const express_1 = __importDefault(require('express'));
-const swagger_ui_express_1 = __importDefault(require('swagger-ui-express'));
-const cors_1 = __importDefault(require('cors'));
-const cookie_parser_1 = __importDefault(require('cookie-parser'));
-const swagger_json_1 = __importDefault(require('./swagger.json'));
-const routes_1 = __importDefault(require('./routes/routes'));
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+const cors_1 = __importDefault(require("cors"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const swagger_json_1 = __importDefault(require("./swagger.json"));
+const routes_1 = __importDefault(require("./routes/routes"));
 const server = (0, express_1.default)();
-server.use(
-  '/documentation',
-  swagger_ui_express_1.default.serve,
-  swagger_ui_express_1.default.setup(swagger_json_1.default),
-);
+server.use('/documentation', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_json_1.default));
 server.use((0, cors_1.default)());
 server.use((0, cookie_parser_1.default)());
 server.use(express_1.default.json());
 server.use(routes_1.default);
 server.listen('8080', () => {
-  console.log('SERVER RUNNING ON PORT 8080');
+    console.log('SERVER RUNNING ON PORT 8080');
 });
--- src/models/connection/connection.ts
+++ src/models/connection/connection.ts
@@ -3,6 +3,6 @@ import dotEnv from 'dotenv';
 
 dotEnv.config({ path: `${__dirname}/../../.env` });
 
-const connection = new Sequelize(`${process.env.DATABASE_URL}`);
+const connection = new Sequelize(`${process.env.DATABASE_URL}`, { ssl: true });
 
 export default connection;
